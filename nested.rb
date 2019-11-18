c6259f127ef04b36ad513c89f14830b5101ae4b4
Nikane Mallea <nikane@mac.com>
2019-11-18 13:14:23 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,7 +1,6 @@
 
 def hopper
-	programmer_hash = 
- 		{
+	programmer_hash = {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -15,12 +14,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
 end
 
-def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+programmer_hash[:grace_hopper]
+
+programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -58,11 +56,8 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => "LISP", "C"}
+end
 
 	programmer_hash = 
  		{
@@ -99,10 +94,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+  programmer_hash ={:alan_kay =>{:known_for =>"GUI"}}
 end
 
 def adding_to_dennis
@@ -121,5 +113,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash = {:dennis_ritchie => {:languages => ["C","Assembly"]}}
 end
+
