ee199c6612b0684b22248b90721d26b2dd2007ea
Nikane Mallea <nikane@mac.com>
2019-11-18 13:09:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	p programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
